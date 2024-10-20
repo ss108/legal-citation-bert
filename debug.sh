e7568c73386093e4a1b147c628b3b6b38c3fffe4
Samier <samier_saeed@protonmail.com>
2024-10-20 07:35:38 -0700

    rm comments from debug.sh

--- debug.sh
+++ debug.sh
@@ -8,8 +8,6 @@ then
   exit 1
 fi
 
-# Extract the script name from the argument
 SCRIPT_NAME=$1
 
-# Start the debugpy server and run the provided script
 python -m debugpy --listen 0.0.0.0:5678 --wait-for-client $SCRIPT_NAME
