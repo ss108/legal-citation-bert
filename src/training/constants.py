6fe423a0e8b2a22898019baf125f11db149f090e
samier <samier@trellis.law>
2024-10-20 09:50:50 -0700

    constants

---
+++ src/training/constants.py
@@ -0,0 +1,35 @@
+
+MODEL_NAME = "dslim/bert-base-NER"
+
+CASE_LABELS = [
+    "B-CASE_NAME",
+    "I-CASE_NAME",
+    "B-VOLUME",
+    "I-VOLUME",
+    "B-REPORTER",
+    "I-REPORTER",
+    "B-PAGE",
+    "I-PAGE",
+    "B-PIN",
+    "I-PIN",
+    "B-COURT",
+    "I-COURT",
+    "B-YEAR",
+    "I-YEAR",
+]
+
+STAT_LABELS = [
+    "B-TITLE",
+    "I-TITLE",
+    "B-CODE",
+    "I-CODE",
+    "B-SECTION",
+    "I-SECTION",
+]
+
+SHORT_LABELS = ["B-ID", "I-ID", "B-SUPRA", "I-SUPRA"]
+
+
+ALL_LABELS = CASE_LABELS + STAT_LABELS + SHORT_LABELS + ["O"]
+
+LABEL_MAP = {label: i for i, label in enumerate(ALL_LABELS)}
--- src/training/model.py
+++ src/training/model.py
@@ -9,41 +9,8 @@ from transformers import (
     PreTrainedTokenizerFast,
 )
 from wasabi import msg
+from src.training.constants import ALL_LABELS, MODEL_NAME
 
-MODEL_NAME = "dslim/bert-base-NER"
-
-CASE_LABELS = [
-    "B-CASE_NAME",
-    "I-CASE_NAME",
-    "B-VOLUME",
-    "I-VOLUME",
-    "B-REPORTER",
-    "I-REPORTER",
-    "B-PAGE",
-    "I-PAGE",
-    "B-PIN",
-    "I-PIN",
-    "B-COURT",
-    "I-COURT",
-    "B-YEAR",
-    "I-YEAR",
-]
-
-STAT_LABELS = [
-    "B-TITLE",
-    "I-TITLE",
-    "B-CODE",
-    "I-CODE",
-    "B-SECTION",
-    "I-SECTION",
-]
-
-SHORT_LABELS = ["B-ID", "I-ID", "B-SUPRA", "I-SUPRA"]
-
-
-ALL_LABELS = CASE_LABELS + STAT_LABELS + SHORT_LABELS + ["O"]
-
-LABEL_MAP = {label: i for i, label in enumerate(ALL_LABELS)}
 
 
 def get_base_model():
